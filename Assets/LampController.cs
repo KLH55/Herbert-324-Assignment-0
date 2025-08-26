8cd58f7ee030e2c5c6776b95e97b670a29b72028
Kristopher Herbert <klh55@pct.edu>
2025-08-25 22:14:45 -0400

    Comment added to code

--- Assets/LampController.cs
+++ Assets/LampController.cs
@@ -1,7 +1,9 @@
+// Kristopher Herbert; LampController is a trigger for when the player gets close to the lamps and will emit particles.
 using UnityEngine;
 
 public class LampController : MonoBehaviour
 {
+    // public variable for the particle system
     public ParticleSystem party;
     private void OnTriggerEnter(Collider collision)
     {
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,11 +1,16 @@
+// Kristopher Herbert; PlayerController is used to control the player movement.
+// Use W and S to move forward and backwards respectively.
+// Use A and D to turn left and right respectively.
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // private variables for movement speed and rotation speed.
     private float speed = 3.0f;
     private float rotSpeed = 90.0f;
 
+    // private variable for the character controller.
     private CharacterController controller;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,6 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // This all is used in conjunction with the SimpleMove function to make the player move.
         float horzInput = Input.GetAxis("Horizontal");
         float vertInput = Input.GetAxis("Vertical");
 
